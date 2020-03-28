35966b8ac6716cfddcb4b1d5ebb9aae6fe418eb4
monaxblack <testreset0054@gmailcom>
2020-03-28 23:57:17 +0300

    3.3. Разбор первой программы.[#262497]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Gorbachev Sergey");
+        System.out.println("00.00.00");
+    }
+}
